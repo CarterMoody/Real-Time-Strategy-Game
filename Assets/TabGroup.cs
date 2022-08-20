2e33f8c600eb963e29c7ed8e3001f5413f8dacbd
Carter Moody <45612260+CarterMoody@users.noreply.github.com>
2022-08-20 09:12:11 -0700

    migrated UI to tab system

---
+++ Assets/TabGroup.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public Sprite tabIdle;
+    public Sprite tabHover;
+    public Sprite tabActive;
+    public TabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+
+    public void Subscribe(TabButton button)
+    {
+        if(tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+        }
+
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button)
+    {
+        ResetTabs();
+        if(selectedTab == null || button != selectedTab)
+        {
+            button.background.sprite = tabHover;
+            TooltipFollow.ShowTooltip_Static(button.toolTipText);
+        }
+        
+    }
+
+
+    public void OnTabExit(TabButton button)
+    {
+        TooltipFollow.HideTooltip_Static(); 
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        if(selectedTab != null)
+        {
+            selectedTab.Deselect();
+        }
+
+        selectedTab = button;
+
+        selectedTab.Select();
+
+        ResetTabs();
+        button.background.sprite = tabActive;
+        TooltipFollow.HideTooltip_Static(); 
+        SetOtherChildrenInactive(button);
+
+    }
+
+    private void SetOtherChildrenInactive(TabButton button)
+    {
+        int index = button.transform.GetSiblingIndex();
+        for(int i = 0; i<objectsToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }
+            else
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+    private void SetAllChildrenInactive()
+    {
+        for(int i = 0; i<objectsToSwap.Count; i++)
+        {
+            objectsToSwap[i].SetActive(false);
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach(TabButton button in tabButtons)
+        {
+            if(selectedTab!=null && button == selectedTab) { continue; }
+            button.background.sprite = tabIdle;
+        }
+    }
+
+    public void Start()
+    {
+        ResetTabs();
+        SetAllChildrenInactive();
+    }
+}
